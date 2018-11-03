31de83e4bf878bc471ccb7a959480d2bc03eee60
Tori Mason <tori.mason@sbcglobal.net>
2018-11-03 11:41:18 -0500

    server.js added

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static("client/public"));
+
+app.get('/', function (req, res) {
+  res.send("Hello World");
+});
+
+const userRoutes = require("./routes/users.js");
+app.use(userRoutes);
+
+const authRoutes = require("./routes/auth.js");
+app.use(authRoutes);
+
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/reactfullstack");
+
+app.listen(PORT, function(){
+  console.log(`Connected on PORT ${PORT}.`);
+});
